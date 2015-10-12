b6b04f80285f09aa71a6d89b076efa739340f283
James Y. Rauhut <jamesrauhut@us.ibm.com>
2015-10-11 21:15:20 -0500

    Started to mix real elements with intro

--- js/sky.js
+++ js/sky.js
@@ -2,15 +2,22 @@ var sloganAnimation = document.querySelector('.sky__title__slogan').animate([
     {opacity: '0'},
     {opacity: '1'}
 ], {
-    duration: 2000
+    duration: 2000,
 });
+
+sloganAnimation.onfinish = function() {
+  document.querySelector('.sky__title__slogan').style.opacity = '1';
+};
+
 sloganAnimation.pause();
 
 var logoAnimation = document.querySelector('.sky__title__logo').animate([
     {transform: 'translateY(100vh)'},
     {transform: 'translateY(0)'}
 ], {
-    duration: 5000
+    duration: 3000,
+    easing: 'ease-in'
 }).onfinish = function() {
+  document.querySelector('.sky__title__logo').style.transform = 'translateY(0)';
   sloganAnimation.play();
 };
