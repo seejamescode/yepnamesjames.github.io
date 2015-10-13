1f4bae7af4286ae457e5e2bee9d5b1908f66cca0
James Y. Rauhut <jamesrauhut@us.ibm.com>
2015-10-13 00:15:37 -0500

    Added an airplane because  web animations api is fun

--- js/sky.js
+++ js/sky.js
@@ -1,3 +1,15 @@
+var airplaneAnimation = document.querySelector('.sky__airplane').animate([
+    {transform: 'scale(0.15) translate(0, 0)'},
+    {transform: 'scale(0.30) translate(-1000%, -100%)'},
+    {transform: 'scale(0.30) translate(-1000%, -100%)'}
+], {
+    duration: 20000,
+    delay: 5000,
+    iterations: Infinity
+});
+
+airplaneAnimation.pause();
+
 var sloganAnimation = document.querySelector('.sky__title__slogan').animate([
     {opacity: '0'},
     {opacity: '1'}
@@ -7,13 +19,14 @@ var sloganAnimation = document.querySelector('.sky__title__slogan').animate([
 
 sloganAnimation.onfinish = function() {
   document.querySelector('.sky__title__slogan').style.opacity = '1';
+  airplaneAnimation.play();
 };
 
 sloganAnimation.pause();
 
 var logoAnimation = document.querySelector('.sky__title__logo').animate([
-    {transform: 'translateY(100vh)'},
-    {transform: 'translateY(0)'}
+    {transform: 'scale(0.75) translateY(100vh)'},
+    {transform: 'scale(1) translateY(0)'}
 ], {
     duration: 3000,
     easing: 'ease-in'
