73f6e0071ae05dfbfdc7a72ef9dc97f5d5b3d7b1
James Y. Rauhut <jamesrauhut@us.ibm.com>
2015-11-21 18:11:15 -0600

    Added three animations

--- js/sky.js
+++ js/sky.js
@@ -1,14 +1,35 @@
 var airplaneAnimation = document.querySelector('.sky__airplane').animate([
-    {transform: 'scale(0.15) translate(0, 0)'},
-    {transform: 'scale(0.30) translate(-1000%, -100%)'},
-    {transform: 'scale(0.30) translate(-1000%, -100%)'}
+    {transform: 'scale(1) translate(0, 0)'},
+    {transform: 'scale(1.2) translate(-425%, -50%)'}
 ], {
-    duration: 20000,
-    delay: 5000,
+    duration: 5000,
     iterations: Infinity
 });
 
-airplaneAnimation.pause();
+var fighterAnimation = document.querySelector('.sky__fighter').animate([
+    {transform: 'scale(1) translate(0, 0) rotateZ(0)'},
+    {transform: 'scale(1.2) translate(650%, 50%) rotateZ(-15deg)'}
+], {
+    duration: 2000,
+    iterations: Infinity
+});
+
+var manAnimation = document.querySelector('.sky__man').animate([
+    {transform: 'scale(1) translate(0, 0) rotateZ(-20deg)'},
+    {transform: 'scale(1.5) translate(-750%, -500%) rotateZ(-10deg)'}
+], {
+    duration: 2000,
+    iterations: Infinity
+});
+
+var bikeAnimation = document.querySelector('.sky__bike').animate([
+    {transform: 'translate(0, 0) rotateZ(0)'},
+    {transform: 'translate(1100%, -200%) rotateZ(10deg)'},
+    {transform: 'translate(2200%, 0) rotateZ(30deg)'}
+], {
+    duration: 5000,
+    iterations: Infinity
+});
 
 var sloganAnimation = document.querySelector('.sky__title__slogan').animate([
     {opacity: '0'},
